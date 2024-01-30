775f1156eb5e7542d0c90053ffd217b2228ae5bb
DNCoding2112 <84671419+DNCoding2112@users.noreply.github.com>
2024-01-30 17:02:11 +0530

    Added index.html, style.css and app.js

---
+++ app.js
@@ -0,0 +1,75 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let turnO=true;
+const msg=document.getElementById("msg");
+const msgCon = document.querySelector(".msg-con");
+const newGameBtn=document.getElementById("new");
+const ResetGameBtn=document.getElementById("res");
+const winptn=[
+[0,1,2],
+[0,3,6],
+[3,4,5],
+[6,7,8],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+];
+
+boxes.forEach((box) =>{
+    box.addEventListener("click",()=>{
+        console.log("Box was clicked");
+        if(turnO===true){
+            box.innerText="O";
+            turnO=false;
+        }
+        else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+
+        checkWinner();
+    })
+})
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const showWin=(winner)=>{
+    msg.innerText=`${winner} is the Winner!`;
+    msgCon.classList.remove("hide"); 
+    disableBoxes(); 
+}
+
+function checkWinner(){
+    for(let pattern of winptn){
+        let p1V=boxes[pattern[0]].innerText;
+        let p2V=boxes[pattern[1]].innerText;
+        let p3V=boxes[pattern[2]].innerText;
+
+        if(p1V!="" && p2V!="" && p3V!="" && p1V===p2V && p2V===p3V & p3V===p1V){
+            console.log("Winner is: ",p1V);
+            showWin(p1V);
+        }
+    }
+}
+
+function resetGame(){
+    turnO=true;
+    enableBoxes();
+    msgCon.classList.add("hide");
+}
+
+newGameBtn.addEventListener("click", resetGame);
+ResetGameBtn.addEventListener("click", resetGame);
